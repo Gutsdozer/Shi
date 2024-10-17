227b87d09a60e9c6e0083a2dd02043fd6b171138
Gutsdozer <warderbass@yandex.ru>
2024-10-17 18:06:15 +0300

    phone number method

--- pages/regpage.py
+++ pages/regpage.py
@@ -1,8 +1,19 @@
-from selenium.webdriver.support.wait import WebDriverWait
+import time
+
 from conftest import browser
 from selenium.webdriver.common.by import By
 from faker import Faker
-from selenium.webdriver.support import expected_conditions as EC
+import random
+
+
+def generate_phone_number(pattern):
+    number = ""
+    for char in pattern:
+        if char == '3':
+            number += str(random.randint(0, 9))
+        else:
+            number += char
+    return number
 
 
 class Regpage:
@@ -10,8 +21,10 @@ class Regpage:
     def __init__(self, browser):
         self.browser = browser
 
-
     def fill_reg_form(self):
+        reg_button = self.browser.find_element(By.XPATH, '//*[@type = "submit"]')
+        assert reg_button is not None
+
         fake = Faker()
         first_name_line = self.browser.find_element(By.XPATH, '//*[@name = "first_name"]')
         first_name_line.click()
@@ -21,6 +34,16 @@ class Regpage:
         last_name_line.click()
         last_name_line.clear()
         last_name_line.send_keys(fake.last_name())
-        reg_button = self.browser.find_element(By.XPATH, '//*[@type = "submit"]')
-        assert reg_button is not None
+        email_line = self.browser.find_element(By.ID, 'email')
+        email_line.click()
+        email_line.clear()
+        email_line.send_keys(fake.email())
+        phone_line = self.browser.find_element(By.ID, 'phone')
+        phone_line.click()
+        phone_line.clear()
+        phone_line.send_keys(generate_phone_number('0532283819'))
+        time.sleep(5)
+
+
+
 
